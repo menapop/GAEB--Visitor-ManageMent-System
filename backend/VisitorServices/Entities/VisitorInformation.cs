6764fcb6b21068fb73f8305a041a4755315b626c
Mena Nasser <menanasser234@gmail.com>
2020-09-22 12:59:41 +0200

    backend

---
+++ backend/VisitorServices/Entities/VisitorInformation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorServices.Entities
+{
+
+    public class VisitorInformation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string IdNumber { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Governrate { get; set; }
+        public string Address { get; set; }
+        public BindUser BindUser { get; set; }
+    }
+}
---
+++ backend/VisitorServices/Migrations/20200922105135_Add Entities To Database.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VisitorServices.Migrations
+{
+    public partial class AddEntitiesToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VisitorInformations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    IdNumber = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Governrate = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VisitorInformations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BindUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Reason = table.Column<string>(nullable: true),
+                    VisitorInformationId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BindUsers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BindUsers_VisitorInformations_VisitorInformationId",
+                        column: x => x.VisitorInformationId,
+                        principalTable: "VisitorInformations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BindUsers_VisitorInformationId",
+                table: "BindUsers",
+                column: "VisitorInformationId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BindUsers");
+
+            migrationBuilder.DropTable(
+                name: "VisitorInformations");
+        }
+    }
+}
