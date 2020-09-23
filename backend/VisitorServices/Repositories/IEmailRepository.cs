90b9f19f4884a07965934ae9a229db63410f187f
mena morcos <menamorcos119@outlook.com>
2020-09-23 11:10:35 +0200

    send email

---
+++ backend/VisitorServices/Repositories/IEmailRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VisitorServices.Repositories
+{
+   public  interface IEmailRepository
+    {
+        bool SendEmail(string To, String Messagebody);
+    }
+}
