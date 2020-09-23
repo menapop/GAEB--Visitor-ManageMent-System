ad365a8bb555aefbd1c2dba169bdb88bfe795af9
Mena Nasser <menanasser234@gmail.com>
2020-09-23 16:53:07 +0200

    change

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
+import { SecuriedPagesGuard } from './shared/Security/securied-pages.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
     path: 'search',
-    loadChildren: () => import('./Components/search/search.module').then(m => m.SearchModule)
+    loadChildren: () => import('./Components/search/search.module').then(m => m.SearchModule),
   },
   {
     path: 'register',
-    loadChildren: () => import('./Components/registeration/registeration.module').then(m => m.RegisterationModule)
+    loadChildren: () => import('./Components/registeration/registeration.module').then(m => m.RegisterationModule),
   },
   {
     path: 'blacklist',
@@ -16,8 +17,13 @@ const routes: Routes = [
   },
   {
     path: 'request',
-    loadChildren: () => import('./Components/request/request.module').then(m => m.RequestModule)
-  }
+    loadChildren: () => import('./Components/request/request.module').then(m => m.RequestModule),
+    canActivate: [SecuriedPagesGuard]
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./Components/search/search.module').then(m => m.SearchModule),
+  },
 
 ];
 
